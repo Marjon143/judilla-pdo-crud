b5ff6cc4e52cf7884431800b49e376b2a9643b32
Marjon143 <marjonjudilla143@gmail.com>
2024-04-15 21:51:31 +0800

    revised db name from marjon to jun

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'marjon');
+define('DB_NAME', 'jun');
  
 /* Attempt to connect to MySQL database */
 try{
