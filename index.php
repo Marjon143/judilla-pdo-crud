39fefd297310b5e320d9daddfc03bebe692f40a4
Marjon143 <marjonjudilla143@gmail.com>
2024-04-29 17:14:46 +0800

    revised index code

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: ../public/welcome.php");
     exit;
 }
  
@@ -59,7 +59,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: ../public/welcome.php");
                             exit;
                         } else{
                             // Password is not valid, display a generic error message
@@ -120,8 +120,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-        </form>-
+            <p>Don't have an account? <a href="./public/register.php">Sign up now</a>.</p>
+        </form>
     </div>
 </body>
 </html>
